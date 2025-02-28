cbde64cba2b77c6c99a726f2407a10a15ff86755
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-02-28 10:51:50 +0530

    [Siddharth] ADD: Conversion of Kilometers in Miles java program

---
+++ KilometersToMiles.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class KilometersToMiles{
+	public static void main(String [] args){
+		double distanceInKilometers = 10.8;
+		double distanceInMiles = distanceInKilometers * (1.6);
+		System.out.print("Equivalent distance in Miles is : " + distanceInMiles);
+	}
+}
