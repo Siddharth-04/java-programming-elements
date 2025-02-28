145ab2df1ea779d9934354295da5ced7a9b768b1
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-02-28 10:50:51 +0530

    [Siddharth] ADD: Average Marks of Sam in PCM java program

---
+++ AveragePercentMarksInPCM.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AverageMarksInPCM{
+	public static void main(String [] args){
+		int marksInMaths = 94;
+		int marksInPhysics = 95;
+		int marksInChemistry = 96;
+		double averageMarksInPCM = (marksInMaths + marksInPhysics + marksInChemistry)/3.0;
+		System.out.print("Average percent of Sam's is : " + averageMarksInPCM);
+	}
+}
